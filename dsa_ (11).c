2b8f36027afd548fa99db8b1a6919f66ec5e4ab3
SHATAKSHI SHARMA <shatakshisharma@SHATAKSHIs-MacBook-Air.local>
2026-04-30 13:52:25 +0530

    DSA Challenge - Day 3

---
+++ dsa_ (11).c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main()
+ {
+    int m, n;
+    scanf("%d %d", &m, &n);
+
+    int matrix[m][n];
+    for (int i = 0; i < m; i++)
+        for (int j = 0; j < n; j++)
+            scanf("%d", &matrix[i][j]);
+
+    if (m!=n)
+     {
+        printf("Not a Symmetric Matrix");
+        return 0;
+    }
+
+    int isSymmetric = 1;
+    for (int i = 0; i < m; i++) 
+    {
+        for (int j = 0; j < n; j++)
+         {
+            if (matrix[i][j] != matrix[j][i]) 
+            {
+                isSymmetric = 0;
+                break;
+            }
+        }
+        if (!isSymmetric) break;
+    }
+
+    if (isSymmetric)
+        printf("Symmetric Matrix");
+    else
+        printf("Not a Symmetric Matrix");
+
+    return 0;
+}
